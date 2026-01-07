83c491e005c003acd985fef20cf3e2ee4e21e711
Dibbiii <alessandra.dibella@studio.unibo.it>
2026-01-07 18:32:59 +0100

    non va uff

--- src/app/components/server-table-component/server-table-component.ts
+++ src/app/components/server-table-component/server-table-component.ts
@@ -5,11 +5,12 @@ import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag';
 import { PrimeTemplate } from 'primeng/api';
 import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-server-table-component',
   standalone: true,
-  imports: [ButtonModule, TableModule, TagModule, PrimeTemplate, CommonModule],
+  imports: [ButtonModule, TableModule, TagModule, PrimeTemplate, CommonModule, RippleModule],
   templateUrl: './server-table-component.html',
   styleUrl: './server-table-component.scss',
 })
@@ -17,7 +18,6 @@ export class ServerTableComponent implements OnInit {
   users: User[] = [];
   allUsersFromDatabase: User[] = [];
   totalRecords: number = 0;
-  isLoading: boolean = false;
 
   ngOnInit(): void {
     // Usa Array.from per generare 100 elementi
@@ -30,41 +30,46 @@ export class ServerTableComponent implements OnInit {
     }));
 
     this.totalRecords = this.allUsersFromDatabase.length;
-    this.isLoading = true;
   }
 
   loadUsers(event: TableLazyLoadEvent) {
-    this.isLoading = true;
     setTimeout(() => {
-      let data = [...this.allUsersFromDatabase];
-      //[...] Prende tutti gli elementi dell'array originale e li "spalma" dentro un **nuovo** array.
-      // Senza i puntini data non sarebbe un nuovo array, ma solo un "soprannome" (riferimento) per
-      // l'array originale. Se ordino `data`, modifico direttamente anche `allUsersFromDatabase`
-      if (event.sortField) {
-        //event.sortField è il nome della colonna (nome o email)
-        data.sort((data1: any, data2: any) => {
-          const field = event.sortField as string; //metto as string perchè sortField potrebbe anche essere un Array nel caso in cui ammettessimo il multisort, però nel nostro caso ordiniamo sempre o in base al nome o in base alla mail, mai in base a entrambi e quindi sortField sarà sempre una stringa
-          let value1 = data1[field];
-          let value2 = data2[field];
-          let result = 0;
+      try {
+        let data = [...this.allUsersFromDatabase];
+        //[...] Prende tutti gli elementi dell'array originale e li "spalma" dentro un **nuovo** array.
+        // Senza i puntini data non sarebbe un nuovo array, ma solo un "soprannome" (riferimento) per
+        // l'array originale. Se ordino `data`, modifico direttamente anche `allUsersFromDatabase`
 
-          if (value1 == null && value2 != null) result = -1;
-          else if (value1 != null && value2 == null) result = 1;
-          else if (value1 == null && value2 == null) result = 0;
-          else if (typeof value1 === 'string' && typeof value2 === 'string')
-            result = value1.localeCompare(value2);
-          else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
+        if (event.sortField) {
+          //event.sortField è il nome della colonna (nome o email)
+          data.sort((data1: any, data2: any) => {
+            const field = event.sortField as string;
+            //metto as string perchè sortField potrebbe anche essere un Array nel caso
+            // in cui ammettessimo il multisort, però nel nostro caso ordiniamo sempre
+            // o in base al nome o in base alla mail, mai in base a entrambi e quindi sortField sarà sempre una stringa
+            let value1 = data1[field];
+            let value2 = data2[field];
+            let result = 0;
 
-          return event.sortOrder === -1 ? -result : result;
-        });
+            if (value1 == null && value2 != null) result = -1;
+            else if (value1 != null && value2 == null) result = 1;
+            else if (value1 == null && value2 == null) result = 0;
+            else if (typeof value1 === 'string' && typeof value2 === 'string')
+              result = value1.localeCompare(value2);
+            else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
+
+            return event.sortOrder === -1 ? -result : result;
+          });
+        }
+
+        const start = event.first || 0;
+        const end = start + (event.rows || 10);
+
+        this.users = data.slice(start, end);
+      } catch (error) {
+        console.error('Errore durante il caricamento:', error);
       }
-
-      const start = event.first || 0;
-      const end = start + (event.rows || 10);
-
-      this.users = data.slice(start, end);
-      this.isLoading = false;
-    }, 10);
+    }, 1000);
   }
 
   editUser(user: User) {
